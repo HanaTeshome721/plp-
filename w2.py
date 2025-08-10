d704c2f4c8815576b0630c6df8cd407dcfc0b78e
HanaTeshome721 <hana2729tm@gmail.com>
2025-08-10 16:22:42 +0300

    feat:addw2 assignment

---
+++ w2.py
@@ -0,0 +1,17 @@
+#1
+my_list=[]
+#2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#3
+my_list.insert(1,15)
+#4
+my_list.extend([50,60,70])
+#5
+my_list.pop()
+#6
+my_list.sort()
+#7
+print(my_list.index(30))
